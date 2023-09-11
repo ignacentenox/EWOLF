798a1114a242871e6be7994b7b98abeb7954d253
Ignacio Manuel Centeno <ignacenteno46@gmail.com>
2023-09-11 10:01:06 -0300

    Funciones js.
    Reinicia el video al principio

--- scripts.js
+++ scripts.js
@@ -1,6 +1,21 @@
-// JavaScript opcional para personalizar el comportamiento del reproductor de video
+<video controls loop autoplay id="videoPlayer">
+  <source src="tu_video.mp4" type="video/mp4">
+    Tu navegador no admite la reproducción de video.</video>
+
+
+videoPlayer.addEventListener('ended', function () {
+  videoPlayer.currentTime = 0; // Reinicia el video al principio
+  videoPlayer.play();
+});
+
 
 const videoPlayer = document.getElementById('videoPlayer');
-
-// Puedes agregar más funcionalidad JavaScript aquí, como eventos personalizados, reproducción automática, etc.
+const playButton = document.getElementById('playButton');
+playButton.addEventListener('click', function () {
+  if (videoPlayer.paused) {
+    videoPlayer.play();
+  } else {
+    videoPlayer.pause();
+  }
+});
 
